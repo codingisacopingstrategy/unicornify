13ace97223be61d42778dc73d6307a1b91ccef53
Eric Schrijver <eric@ericschrijver.nl>
2026-05-07 22:17:53 +0200

    Example and committed the example output too
    This way we can check if the algorithm stays consistent

--- example.py
+++ example.py
@@ -1,9 +1,20 @@
 import hashlib
 from avatar import create_avatar
+from sys import argv
 
-hash = hashlib.md5(b"your.email@address.here").hexdigest()
 
-# Creates a BMP file of 256x256 pixels (see docstring of create_avatar)
-f = open("unicorn.bmp", "wb")
-f.write(create_avatar(128, int(hash, 16)))
-f.close()
+def unicorn_for_email(email):
+    hash = hashlib.md5(email).hexdigest()
+    # Creates a BMP file of 256x256 pixels (see docstring of create_avatar)
+    f = open("%s.bmp" % email.decode('utf-8'), "wb")
+    f.write(create_avatar(128, int(hash, 16)))
+    f.close()
+
+
+if __name__ == "__main__":
+    """Usage: python3 example.py
+       Optional argument: e-mail address."""
+    address = b'alice@example.com'
+    if len(argv) == 2:
+        address = argv[1].encode('utf-8')
+    unicorn_for_email(address)
